d27b4b93e8990aee83e3adf46dcb34bd752fe54f
bento1 <bentotoda@naver.com>
2024-09-18 13:12:09 +0900

    test
    test

---
+++ encoder_decoder/Pretraining_T5_custom_dataset/clena_text.py
@@ -0,0 +1,39 @@
+import re
+
+# Basic Cleaning Text Function
+def CleanText(readData, Num=False, Eng=False):
+
+    # Remove Retweets 
+    text = re.sub('RT @[\w_]+: ', '', readData)
+
+    # Remove Mentions
+    text = re.sub('@[\w_]+', '', text)
+
+    # Remove or Replace URL 
+    text = re.sub(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", ' ', text) # http로 시작되는 url
+    text = re.sub(r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", ' ', text) # http로 시작되지 않는 url
+    
+    # Remove Hashtag
+    text = re.sub('[#]+[0-9a-zA-Z_]+', ' ', text)
+
+    # Remove Garbage Words (ex. &lt, &gt, etc)
+    text = re.sub('[&]+[a-z]+', ' ', text)
+
+    # Remove Special Characters
+    text = re.sub('[^0-9a-zA-Zㄱ-ㅎ가-힣]', ' ', text)
+    
+    # Remove newline
+    text = text.replace('\n',' ')
+    
+    if Num is True:
+        # Remove Numbers
+        text = re.sub(r'\d+',' ',text)
+    
+    if Eng is True:
+        # Remove English 
+        text = re.sub('[a-zA-Z]' , ' ', text)
+
+    # Remove multi spacing & Reform sentence
+    text = ' '.join(text.split())
+       
+    return text
